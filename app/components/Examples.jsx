37fbfdc2f171f548afe6eb89af9cb4064e547c2a
Shawn Vader <svader@gmail.com>
2016-12-18 10:51:24 +0000

    Updated title on examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-     <h4>Examples component</h4>
+     <h4>Examples</h4>
      <p>
        Welcome to components
      </p>
