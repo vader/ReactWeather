37a11846141e9c9151901b331bfe3b1fd33531b3
Shawn Vader <svader@gmail.com>
2016-12-22 16:13:13 +0000

    Add error Modal

--- api/WeatherMapApi.jsx
+++ api/WeatherMapApi.jsx
@@ -4,17 +4,18 @@ const OPEN_WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?APPID=e
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl = `${OPEN_WEATHER_URL}&q=${encodedLocation}`;
+    var requestUrl = `${OPEN_WEATHER_URL}q=${encodedLocation}}`;
 
-    return axios.get(requestUrl).then(function (res) {
+    axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
-        console.log(res.data.main.temp);
         return res.data.main.temp;
       }
-    }, function (err) {
-      throw new Error('Unable to fetch weather for that location');
+    },
+
+    function (res) {
+      throw new Error(res.data.message);
     });
   },
 };
